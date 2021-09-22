80a3608c236b97b68aa437f8ae8750831c44a524
YQZ530 <yongqizhang530@gmail.com>
2021-09-22 10:06:53 -0400

    add scenes.

---
+++ Assets/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof( Animator))]
+public class GameController : MonoBehaviour
+{
+    enum GameState { Idle, Walk, Jump};
+    GameState curState = GameState.Idle;
+    Animator anim;
+    // Start is called before the first frame update
+    int jumpHash = Animator.StringToHash("Jump");
+    int walkHash = Animator.StringToHash("Walk");
+    int idleHash = Animator.StringToHash("Idle");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            int statehash = GetCurState();
+            SetTrigger(statehash);
+        }
+    }
+
+    void SetTrigger(int hashvale)
+    {
+        anim.SetTrigger(hashvale);
+    }
+
+    int GetCurState()
+    {
+        switch (curState)
+        {
+            case GameState.Idle:
+                curState = GameState.Walk;
+                return walkHash;
+               
+            case GameState.Walk:
+                curState = GameState.Jump;
+                return jumpHash;
+            case GameState.Jump:
+                curState = GameState.Idle;
+              
+                return idleHash;
+            default:
+                return idleHash;
+
+        }
+
+
+    }
+}
